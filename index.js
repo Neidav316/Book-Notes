41fca49d4f913b5e2060f12f3c6bebb0b1aff2c4
Neidav316 <neidav86@gmail.com>
2024-11-27 13:38:39 +0200

    added dotenv package for better data security

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import pg from "pg";
-import data from "./secrets.json" assert {type:'json'}
+import env from "dotenv"
+
 
 const app = express();
 const port = 3000;
-const password = data.password;
+env.config();
+
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "booknotes",
-    password: password ? password : "******",
-    port: 5432
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
 });
 
 app.use(bodyParser.urlencoded({extended:true}));
